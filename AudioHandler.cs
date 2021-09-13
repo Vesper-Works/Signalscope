c34c8ad3b8dcc344e59ad976f3dd9d471e3fc96b
Vesper <59376295+Vesper-Works@users.noreply.github.com>
2021-09-13 21:42:56 +0100

    Initial Commit

---
+++ AudioHandler.cs
@@ -0,0 +1,228 @@
+﻿using Discord;
+using Discord.WebSocket;
+using SharpLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode.Common;
+
+namespace DiscordMusicBot
+{
+    public static class AudioHandler
+    {
+        private static LavalinkManager _lavalinkManager { get => Program.Instance.LavalinkManager; }
+
+        public static async Task<LavalinkPlayer> JoinUsersChannel(SocketGuildUser user)
+        {
+            IVoiceChannel voiceChannel = user.VoiceChannel;
+            return _lavalinkManager.GetPlayer(user.Guild.Id) ?? await _lavalinkManager.JoinAsync(voiceChannel);
+        }
+        public static async Task AddSongToQueue(SocketGuildUser user, string query, bool generateQueueEmbed)
+        {
+            LoadTracksResponse response;
+
+            if (query.StartsWith("https"))
+            {
+                response = await _lavalinkManager.GetTracksAsync(query);
+            }
+            else
+            {
+                response = await _lavalinkManager.GetTracksAsync($"ytsearch:" + query);
+            }
+            LavalinkTrack track = response.Tracks.First();
+
+            Program.Instance.SongQueues[user.Guild.Id].Enqueue(track);
+
+            if (generateQueueEmbed) await Program.Instance.ActiveChannels[user.Guild.Id].SendMessageAsync(embed: new EmbedBuilder().CreateQueueEmbed(user).Build());
+
+            if (Program.Instance.SongQueues[user.Guild.Id].Count == 1)
+            {
+                PlayNextSong(user.Guild.Id, user);
+            }
+        }
+        public static async void PlaySong(SocketGuildUser user, string query, ISocketMessageChannel channel)
+        {
+            var player = await JoinUsersChannel(user);
+
+            LoadTracksResponse response;
+
+            if (query.StartsWith("https"))
+            {
+                response = await _lavalinkManager.GetTracksAsync(query);
+            }
+            else
+            {
+                response = await _lavalinkManager.GetTracksAsync($"ytsearch:" + query);
+            }
+            LavalinkTrack track = response.Tracks.First();
+
+            var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Now playing " + track.Title,
+                Description = "By " + track.Author + Environment.NewLine + track.Url,
+                Color = Color.Orange,
+                ImageUrl = "https://img.youtube.com/vi/" + track.Identifier + "/0.jpg"
+            };
+            embed.WithCurrentTimestamp();
+            embed.WithAuthor(user);
+            //Your embed needs to be built before it is able to be sent
+            await channel.SendMessageAsync(embed: embed.Build());
+
+            await player.PlayAsync(track);
+        }
+        public static async void PlaySong(SocketGuildUser user, LavalinkTrack track, ISocketMessageChannel channel)
+        {
+            var player = await JoinUsersChannel(user);
+            var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Now playing " + track.Title,
+                Description = "By " + track.Author + Environment.NewLine + track.Url,
+                Color = Color.Orange,
+                ImageUrl = "https://img.youtube.com/vi/" + track.Identifier + "/0.jpg"
+            };
+            embed.WithCurrentTimestamp();
+            embed.WithAuthor(user);
+            //Your embed needs to be built before it is able to be sent
+            await channel.SendMessageAsync(embed: embed.Build());
+
+            await player.PlayAsync(track);
+        }
+        public static async void PlaySong(LavalinkPlayer player, LavalinkTrack track)
+        {
+            await player.PlayAsync(track);
+
+            var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Now playing " + track.Title,
+                Description = "By " + track.Author + Environment.NewLine + track.Url,
+                Color = Color.Orange,
+                ImageUrl = "https://img.youtube.com/vi/" + track.Identifier + "/0.jpg"
+            };
+            embed.WithCurrentTimestamp();
+            //Your embed needs to be built before it is able to be sent
+            await Program.Instance.ActiveChannels[player.VoiceChannel.GuildId].SendMessageAsync(embed: embed.Build());
+        }
+
+        public static async void LeaveChannel(ulong guildId)
+        {
+            await _lavalinkManager.LeaveAsync(guildId);
+        }
+
+        public static async Task OnTrackEnd(LavalinkPlayer lavalinkPlayer, LavalinkTrack track, string reason)
+        {
+            Console.WriteLine("Song ended:- " + reason);
+            if (reason == "FINISHED")
+            {
+                PlayNextSong(lavalinkPlayer);
+            }
+        }
+
+        public static void PlayNextSong(SocketGuildUser user, ulong guild, ISocketMessageChannel channel)
+        {
+            LavalinkTrack trackToPlay = Program.Instance.SongQueues[guild].NextSong();
+            PlaySong(user, trackToPlay, channel);
+        }
+        public static void PlayNextSong(LavalinkPlayer player)
+        {
+            LavalinkTrack trackToPlay = Program.Instance.SongQueues[player.VoiceChannel.GuildId].NextSong();
+            PlaySong(player, trackToPlay);
+        }
+        public async static void PlayNextSong(ulong guildId, SocketGuildUser user = null)
+        {
+            LavalinkTrack trackToPlay = Program.Instance.SongQueues[guildId].NextSong();
+
+            var player = _lavalinkManager.GetPlayer(guildId);
+            if (player == null)
+            {
+                player = await JoinUsersChannel(user);
+            }
+            PlaySong(_lavalinkManager.GetPlayer(guildId), trackToPlay);
+        }
+        public async static void PlayPreviousSong(ulong guildId, SocketGuildUser user = null)
+        {
+            LavalinkTrack trackToPlay = Program.Instance.SongQueues[guildId].PreviousSong();
+
+            var player = _lavalinkManager.GetPlayer(guildId);
+            if (player == null)
+            {
+                player = await JoinUsersChannel(user);
+            }
+            PlaySong(_lavalinkManager.GetPlayer(guildId), trackToPlay);
+        }
+        public static EmbedBuilder CreateQueueEmbed(this EmbedBuilder embedBuilder, SocketGuildUser user)
+        {
+            string queueString = "";
+
+            foreach (var song in Program.Instance.SongQueues[user.Guild.Id].GetAllTracks())
+            {
+                string title = song.Title.Replace(song.Author, "");
+                string author = song.Author.Replace(" - Topic", "");
+
+                queueString += title + ", by " + author;
+                if (song == Program.Instance.SongQueues[user.Guild.Id].CurrentSong()) { queueString += " (Current Song)"; }
+                queueString += Environment.NewLine;
+            }
+
+            TimeSpan totalLength = new TimeSpan(0);
+
+            foreach (var song in Program.Instance.SongQueues[user.Guild.Id].GetAllTracks())
+            {
+                totalLength += song.Length;
+            }
+
+            var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Current queue:",
+                Description = queueString,
+                Color = Color.Orange
+                //ImageUrl = "https://img.youtube.com/vi/" + track.Identifier + "/0.jpg"
+            };
+            embed.WithCurrentTimestamp();
+            embed.WithFooter(totalLength.ToString());
+            return embed;
+        }
+        public static async Task<string[]> GetVideoLinksFromPlaylist(string playlistLink)
+        {
+
+            var youtube = new YoutubeExplode.YoutubeClient();
+            var playlist = await youtube.Search.GetPlaylistsAsync(playlistLink);
+
+            List<string> urls = new List<string>();
+
+
+
+            foreach (var item in await youtube.Playlists.GetVideosAsync(playlist[0].Id))
+            {
+                urls.Add(item.Url);
+            }
+            return urls.ToArray();
+        }
+        public static async Task<string[]> GetPlaylistVideosAsync(string link)
+        {
+            var youtube = new YoutubeExplode.YoutubeClient();
+            // Get playlist metadata
+            var playlist = await youtube.Playlists.GetAsync(link);
+
+            var title = playlist.Title; // "First Steps - Blender 2.80 Fundamentals"
+            var author = playlist.Author; // "Blender"
+
+            // Get all playlist videos
+            var playlistVideos = await youtube.Playlists.GetVideosAsync(playlist.Id);
+            string[] links = new string[playlistVideos.Count];
+
+            for (int i = 0; i < playlistVideos.Count; i++)
+            {
+                links[i] = playlistVideos[i].Url;
+            }
+
+            return links;
+        }
+    }
+
+}
---
+++ CommandModules.cs
@@ -0,0 +1,134 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using SharpLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DiscordMusicBot
+{
+    // Create a module with no prefix
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        #region Rickroll
+        [Command("rickroll")]
+        [Summary("You know what this does.")]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task RickRollAsync()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+            AudioHandler.PlaySong(user, "https://www.youtube.com/watch?v=dQw4w9WgXcQ", Context.Channel);
+        }
+        #endregion
+
+        [Command("play")]
+        [Summary("Joins the user's channel and plays song")]
+        public async Task PlaySong([Remainder][Summary("Song query to play")] string query)
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+
+
+            if (!Program.Instance.SongQueues.ContainsKey(user.Guild.Id))
+            {
+                Program.Instance.SongQueues.Add(user.Guild.Id, new SongQueue());
+            }
+            if (!Program.Instance.ActiveChannels.ContainsKey(user.Guild.Id))
+            {
+                Program.Instance.ActiveChannels.Add(user.Guild.Id, Context.Channel);
+            }
+            else if (Program.Instance.ActiveChannels[user.Guild.Id] != Context.Channel)
+            {
+                Program.Instance.ActiveChannels[user.Guild.Id] = Context.Channel;
+            }
+
+            if (query.Contains("playlist"))
+            {
+                var links = await AudioHandler.GetPlaylistVideosAsync(query);
+                for (int i = 0; i < links.Length; i++)
+                {
+                    if (i == links.Length-1)
+                    {
+                        await AudioHandler.AddSongToQueue(user, links[i], true);
+                    }
+                    else
+                    {
+                        await AudioHandler.AddSongToQueue(user, links[i], false);
+                    }
+                }
+            }
+            else
+            {
+                await AudioHandler.AddSongToQueue(user, query, true);
+            }
+
+
+        }
+
+        [Command("psong")]
+        [Summary("Plays the previous song in the queue")]
+        public async Task PlayPreviousSong()
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+            AudioHandler.PlayPreviousSong(Context.Guild.Id, user);
+        }
+
+        [Command("nsong")]
+        [Summary("Plays the next song in the queue")]
+        public async Task PlayNextSong()
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+            AudioHandler.PlayNextSong(Context.Guild.Id, user);
+        }
+
+        [Command("queue")]
+        [Summary("Shows the queue")]
+        public async Task ShowQueue()
+        {
+            SocketGuildUser user = Context.User as SocketGuildUser;
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder().CreateQueueEmbed(user).Build());
+
+        }
+
+    }
+
+    // Create a module with the 'sample' prefix
+    [Group("sample")]
+    public class SampleModule : ModuleBase<SocketCommandContext>
+    {
+        // ~sample square 20 -> 400
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync(
+            [Summary("The number to square.")]
+        int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        // ~sample userinfo --> foxbot#0282
+        // ~sample userinfo @Khionu --> Khionu#8708
+        // ~sample userinfo Khionu#8708 --> Khionu#8708
+        // ~sample userinfo Khionu --> Khionu#8708
+        // ~sample userinfo 96642168176807936 --> Khionu#8708
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo")]
+        [Summary
+        ("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+            [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    }
+
+}
